70a96aa4f959e6d1ba51ecd6198527a6eb5b9e60
1764775976@qq.com <1764775976@qq.com>
2020-07-22 20:46:50 +0800

    [OO][richard]feature: Based on exercise 9,klass add a isIn function to judge student in this klass .teacher have attribute klass[] and add judge if teach one student

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,57 @@
 package practice10;
 
-public class Teacher {
+
+import java.util.List;
+
+public class Teacher extends Person {
+    private List<Klass> classes;
+
+    public Teacher(Integer id, String name, Integer age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<Klass> classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String introduce() {
+        if(classes!=null && classes.size()>0){
+            String classList = "";
+            for(Klass klass:classes){
+                classList += klass.getNumber()+", ";
+            }
+            return super.introduce()+" I am a Teacher. I teach Class "+classList.substring(0,classList.length()-2)+".";
+        }
+        return super.introduce()+" I am a Teacher. I teach No Class.";
+    }
+
+
+    public String introduceWith(Student student) {
+        for(Klass klass:classes){
+            if(student.getKlass().getNumber().equals(klass.getNumber())){
+                return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+            }
+        }
+        return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+
+    }
+
+    public boolean isTeaching(Student student) {
+        for(Klass klass:classes){
+            if(klass.isIn(student)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
