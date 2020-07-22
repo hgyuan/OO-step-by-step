3d9cb1bc2b6169f5c395f944ec0c4fdab0c71b11
1764775976@qq.com <1764775976@qq.com>
2020-07-22 19:12:32 +0800

    [OO][richard]feature: create class person ,teacher,student and klass, use the klass.class to express the student's or teacher's class number.

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,19 @@
 package practice07;
 
 public class Klass {
+    private String displayName;
+    private Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+        this.displayName = "Class "+this.number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
 }
