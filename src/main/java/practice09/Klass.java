213dcdc8a011b5b2b7a98258a16158c031d21930
1764775976@qq.com <1764775976@qq.com>
2020-07-22 20:17:20 +0800

    [OO][richard]feature: Based on exercise 8,klass add a appendMember function to add student in this klass .need to judge student is this klass then set to leader

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,51 @@
 package practice09;
 
+
 public class Klass {
+    private String displayName;
+    private Integer number;
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+        this.displayName = "Class "+this.number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void assignLeader(Student student) {
+        if(!student.getKlass().number.equals(this.number)){
+            System.out.printf("It is not one of us.\n");
+            return ;
+        }
+        this.leader = student;
+        return ;
+    }
+
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,43 @@
 package practice09;
 
-public class Teacher {
+
+public class Teacher extends Person {
+
+    private Klass klass;
+
+
+    public Teacher(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(klass!=null){
+            return super.introduce()+" I am a Teacher. I teach Class "+klass.getNumber()+".";
+        }
+        return super.introduce()+" I am a Teacher. I teach No Class.";
+    }
+
+
+    public String introduceWith(Student student) {
+        if(student.getKlass().getNumber().equals(this.klass.getNumber())){
+            return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }
+        return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+
+    }
+
 }
