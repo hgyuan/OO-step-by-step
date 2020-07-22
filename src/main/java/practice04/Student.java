ffb480534561759baf9c2ed5d032aa813c88f5db
1764775976@qq.com <1764775976@qq.com>
2020-07-22 16:59:03 +0800

    "oo[richard]feature: complete exercise 04"

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,24 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
+    }
 }
