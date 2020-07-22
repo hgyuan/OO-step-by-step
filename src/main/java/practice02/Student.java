62258e533866d949cc5ae1b9b49f5a0fdbe79b93
1764775976@qq.com <1764775976@qq.com>
2020-07-22 16:44:51 +0800

    "oo[richard]feature: update the complete exercise 01 and 02"

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,11 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce(){
+        return "My name is Tom. I am 18 years old. Coding for the glory of OOCL.";
+    }
 }
